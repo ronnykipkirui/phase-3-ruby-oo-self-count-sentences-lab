af9eff644eb4e2e42cc3cb710bf414eade93f2b1
Ronny <ronnybet41@gmail.com>
2022-10-23 10:07:22 +0300

    completed assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,27 @@
+
+
 require 'pry'
 
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
+    self.end_with?("!")
 
   end
 
   def count_sentences
-
+    self.split(/\w/).collect{|e| e.strip}.filter {|char| char!=""}.length
   end
 end
+
+
+puts "This is a string! It has three sentences. Right?".count_sentences
